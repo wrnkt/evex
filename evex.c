c47c821bb0614893b88ad31a6b93cf58ffc70880
Niko Ware <wrnkt1@gmail.com>
2023-07-08 05:57:05 -0400

    Separate utility file

--- evex.c
+++ evex.c
@@ -6,33 +6,9 @@
 #include <string.h>
 #include <stdarg.h>
 #include <assert.h>
+#include "./util.c"
 
-void usage(FILE *stream)
-{
-    fprintf(stream, "Enter a simple math expression (no parenthesis) and get the result.\n");
-}
 
-void error(char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    
-    char *err_prefix = "[ERROR]: ";
-    
-    size_t len = 100;
-    char err[len];
-    char msg[len];
-
-    size_t n = vsnprintf(msg, len, fmt, args);
-    assert(n >= 0 && n < len);
-
-    strcat(err, err_prefix);
-    strcat(err, msg);
-    strcat(err, "\n");
-    fputs(err, stderr);
-}
-
-///////////////////////////////////////////////
 
 typedef enum {
     UNDEF = -1,
---
+++ util.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <assert.h>
+
+void usage(FILE *stream)
+{
+    fprintf(stream, "Enter a simple math expression (no parenthesis) and get the result.\n");
+}
+
+void error(char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    
+    char *err_prefix = "[ERROR]: ";
+    
+    size_t len = 100;
+    char err[len];
+    char msg[len];
+
+    size_t n = vsnprintf(msg, len, fmt, args);
+    assert(n >= 0 && n < len);
+
+    strcat(err, err_prefix);
+    strcat(err, msg);
+    strcat(err, "\n");
+    fputs(err, stderr);
+}
