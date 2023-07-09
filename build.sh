72d981aea15e14d830bbe2e1bbfc65bb600848e2
Niko Ware <wrnkt1@gmail.com>
2023-07-08 23:17:50 -0400

    Refactor build script

--- build.sh
+++ build.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-BUILD_LOCATION=dist/evex
+BUILD_LOCATION=./dist/evex;
 
-if [ "$1" == "build" ]
-then
-     echo "[INFO]: building to $BUILD_LOCATION"
-     gcc -g evex.c util.c -o $BUILD_LOCATION
-     echo "[INFO]: done."
+build() {
+    echo "[INFO]: building to $BUILD_LOCATION"
+    gcc -g evex.c util.c -o $BUILD_LOCATION
+    echo "[INFO]: done."
+}
 
-elif [ "$1" == "clean" ]
-then
-     echo "[INFO]: removing $BUILD_LOCATION"
-     rm $BUILD_LOCATION
+clean() {
+    echo "[INFO]: removing $BUILD_LOCATION"
+    rm $BUILD_LOCATION
+}
 
-elif [ "$1" == "run" ]
-then
+run() {
     shift
-    ./dist/evex "$@"
+    $BUILD_LOCATION "$@"
 
     if [ $? -ne 0 ]; then
         echo "[ERROR]: execution of $BUILD_LOCATION failed."
         exit 1
     fi
+}
 
+if [ "$1" == "build" ]
+then
+    build
+elif [ "$1" == "clean" ]
+then
+    clean
+elif [ "$1" == "run" ]
+then
+    run $@
 else
     echo "[ERROR]: Unsupported option."
 
